171c6c13ffac468bc22712a62024a3aea4858274
RED-BMX <urbanow05@gmail.com>
2026-08-21 21:46:17 -0500

    test: add unit tests for inventory

---
+++ Sistema-Inventario-CSharp.Tests/FakePersistencia.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sistema_Inventario_CSharp;
+
+namespace Sistema_Inventario_CSharp.Tests;
+
+public class FakePersistencia : IPersistencia
+{
+    private List<Producto> productos = new();
+
+    public void GuardarProductos(IEnumerable<Producto> productos)
+    {
+        this.productos = productos.ToList();
+    }
+
+    public IEnumerable<Producto> CargarProductos()
+    {
+        return productos.AsReadOnly();
+    }
+}
